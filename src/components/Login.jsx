80861eefe448a2caa5519b1566774b91a272c69d
ksvdteja <divyateja773@gmail.com>
2025-03-19 23:50:47 +0530

    can't access other routes without login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [emailId, setEmailId] = useState("user001@gmail.com");
+  const [password, setPassword] = useState("User@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-export default Login
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data));
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card card-border bg-success-content w-96">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                onChange={(e) => setEmailId(e.target.value)}
+                value={emailId}
+                className="input"
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                className="input"
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-end">
+            <button onClick={() => handleLogin()} className="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,40 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const user = useSelector((store) => store.user);
   return (
     <div>
       <div className="navbar bg-info-content shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">Dev Meet</a>
+          <Link to="/" className="btn btn-ghost text-xl">Dev Meet</Link>
         </div>
         <div className="flex gap-2">
           <div className="dropdown dropdown-end mx-5">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
+            {user && (
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="user-photo"
+                    src={user.photoUrl}
+                  />
+                </div>
               </div>
-            </div>
+            )}
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
